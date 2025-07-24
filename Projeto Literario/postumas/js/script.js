0dee1d59a9b655ea05931ae0e5dfcb47b6bdc477
Matheus <133571818+theuzvini@users.noreply.github.com>
2025-07-24 20:50:53 -0300

    -update_24_07_2025-
    +Add pagina inicial
    +Add fonte inria-sans
    +Add som de clique
    +Add som de chuva
    +Add navbar
    +Add restruturação do js

--- Projeto Literario/postumas/js/script.js
+++ Projeto Literario/postumas/js/script.js
@@ -9,3 +9,56 @@ setTimeout(() => {
   document.getElementById('logo').style.display = 'none';
   document.getElementById('jogo').style.display = 'block';
 }, 10000);
+
+document.addEventListener("click", () => {
+  const audio = document.getElementById("fundo");
+  audio.muted = false;
+  audio.volume = 0.1;
+  audio.play().catch(e => console.warn("Falha ao iniciar o áudio:", e));
+}, { once: true });
+
+
+function tocarSom1() {
+  const som = document.getElementById("clique");
+  som.currentTime = 0;
+  som.play();
+  setTimeout(() => {
+    window.location.href = '../html/sobre.html';
+  }, 600);
+}
+
+function tocarSom2() {
+  const som = document.getElementById("clique");
+  som.currentTime = 0;
+  som.play();
+  setTimeout(() => {
+    window.location.href = '../html/tutorial.html';
+  }, 600);
+}
+
+function tocarSom3() {
+  const som = document.getElementById("clique");
+  som.currentTime = 0;
+  som.play();
+  setTimeout(() => {
+    window.location.href = '../html/autoria.html';
+  }, 600);
+}
+
+function tocarSom4() {
+  const som = document.getElementById("clique");
+  som.currentTime = 0;
+  som.play();
+  setTimeout(() => {
+    window.location.href = '../html/dedicatoria.html';
+  }, 600);
+}
+
+function tocarSom5() {
+  const som = document.getElementById("clique");
+  som.currentTime = 0;
+  som.play();
+  setTimeout(() => {
+    window.location.href = '../html/livro.html';
+  }, 600);
+}
